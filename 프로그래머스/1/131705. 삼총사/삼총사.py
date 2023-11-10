09f8785c10d2b2ea874100818cdd61056b6a353e
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-11-10 16:43:36 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,10 @@
+def solution(number):
+    count = 0
+
+    for i in range(0, len(number)):
+        for j in range(i+1, len(number)):
+            for k in range(j+1, len(number)):
+                if (number[i]+number[j]+number[k] == 0):
+                    count += 1
+
+    return count
