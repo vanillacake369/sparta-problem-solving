9362d296178368c95da53a693fec7d7f3b5e30e3
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-11-15 11:18:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -21,16 +21,25 @@ def switch(digStr):
         return '9'
     return None
 
+
 def solution(s):
     result = []
     digStr = ""
+    
     for ch in s:
+        # 문자가 숫자이면 :: result 리스트에 추가
         if(ch.isdigit()):
             result.append(str(ch))
             continue
+        
+        # 문자가 영문이면 :: digStr에 concat
         digStr += ch
+        
+        # digStr가 3을 넘었고, 숫자변환이 가능하다면 :: 숫자변환값 result 리스트에 추가
         if(len(digStr) >= 3):
             if(switch(digStr) != None):
                 result.append(switch(digStr))
                 digStr = ""
+                
+    # 문자로 이루어진 result를 문자열로 만든 뒤, int로 형변환
     return int("".join(result))
