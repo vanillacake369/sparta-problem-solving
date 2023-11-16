3e8699264741f44db8c7e82afb59a79a04ab907e
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-11-16 14:49:28 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = []
+    for i in range(0,len(numbers)):
+        for j in range(i+1,len(numbers)):
+            answer.append(numbers[i]+numbers[j])
+    answer.sort()
+    answer = list(dict.fromkeys(answer))
+    return answer
