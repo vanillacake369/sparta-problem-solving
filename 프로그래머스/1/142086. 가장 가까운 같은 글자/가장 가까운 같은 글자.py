f4f093f383b79d12287b1268dd810c7bdac37f38
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-11-17 11:25:42 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.00 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,6 +1,12 @@
 def solution(s):
     answer = []
     mem = {}
+    # mem[a] = 1
+    # i = 3
+    # s[i] = a
+    # diff = 3 - mem[a] = 3-1 = 2
+    # answer.append(2)
+    # mem[s[i]] = i = mem[a] = 3
     for i in range(0,len(s)):
         if((s[i] in mem) == False):
             answer.append(-1)
