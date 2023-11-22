cf85b9859c74a93fa206f7580b71b9f8a6bc4b09
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-11-22 11:10:16 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,5 +1,9 @@
+# 달력에 대한 값을 잘 세팅할 것,,,,
+# for문을 돌 때 몇번째인덱스까지 포함해서 도는 지 유념할 것,,
 def solution(a, b):
-    days = ['THU', 'FRI', 'SAT','SUN', 'MON', 'TUE','WED']
-    months = [31,29,31,30,31,30, 31, 31, 30, 31, 30, 31]
-    
-    return days[(sum(months[:a-1])+b)%7]
+    cal = [31,29,31,30,31,30,31,31,30,31,30,31]
+    date = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    days = 0
+    for i in range(0,a-1):
+        days += cal[i]
+    return date[(days + b) % 7]
