6f3973776fc33725af8c6a59634843ed5b1b1837
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-12-06 09:54:24 +0900

    [level 1] Title: 둘만의 암호, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,16 +1,21 @@
+# https://school.programmers.co.kr/questions/43553?referer=collection-of-questions
+# 반례 :: skip의 문자열이 연속된 문자열인 경우, 연속해서 더해주어야함
+# abcdef
+
 def solution(s, skip, index):
-    answer = ''
-    
-    for c in s:
-        i = ord(c)
-        j = index
-        while j > 0:
-            i += 1
-            if i > ord('z'):
-                i = ord('a')
-            if chr(i) in skip:
-                j += 1
-            j -= 1
-        answer += chr(i)
-    
-    return answer
+    answer = []
+    skipMap = {}
+    for sk in skip:
+        skipMap.update({ord(sk):sk})
+    for eachS in s:
+        nextS = ord(eachS)
+        i = index
+        while i > 0:
+            nextS += 1
+            if(nextS > 122):
+                nextS = 97
+            if(skipMap.get(nextS)):
+                i += 1
+            i -= 1
+        answer.append(chr(nextS))
+    return "".join(answer)
