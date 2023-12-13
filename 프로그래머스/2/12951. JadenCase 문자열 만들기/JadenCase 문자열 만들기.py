e2ebe7b3207dda232449194430c0ff42a8e8f30e
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-12-13 13:05:01 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = []
+    strs=s.split(" ")
+    for chs in strs : 
+        if(len(chs) != 0):
+            chs = chs[0].upper() + chs[1:].lower()
+        answer.append(chs)
+    return " ".join(answer)
