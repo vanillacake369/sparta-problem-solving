d8b54536f0f5febea184150f82a7af5d4753c0b2
LimJihoon (Tony) <lonelynight1026@gmail.com>
2023-12-28 20:45:54 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,16 @@
+# -- 코드를 입력하세요
+SELECT
+    CONCAT('/home/grep/src/',
+           f.BOARD_ID,'/',
+           f.FILE_ID,
+           f.FILE_NAME,
+           f.FILE_EXT)
+   AS FILE_PATH
+FROM 
+    USED_GOODS_FILE f
+LEFT JOIN 
+    USED_GOODS_BOARD b
+ON 
+    b.BOARD_ID = f.BOARD_ID
+WHERE b.VIEWS =  (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD)
+ORDER BY f.FILE_ID DESC
