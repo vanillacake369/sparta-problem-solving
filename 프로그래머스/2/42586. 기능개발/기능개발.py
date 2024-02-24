9d1022cf10ab465338481b632cf77448c4985cfe
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-02-25 01:52:21 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,22 @@
 import math
-
 def solution(progresses, speeds):
     answer = []
 
     # 1
-    # for 문을 사용하여 구한 필요한작업기간리스트
-    # leftProgresses = []
-    # for idx,p in progresses:
-    #     leftProgresses.append((100 - p) // speeds[idx])
-    # list comprehension 을 사용하여 구한 필요한작업기간리스트
     leftProgresses = [math.ceil((100 - p) / speeds[idx]) for idx, p in enumerate(progresses)]
 
-    # 2
     leftPStack = [leftProgresses[0]]
     daysToDeploy = 0
-    for pr in leftProgresses:
+    for idx,pr in enumerate(leftProgresses):
+        # 2
         if(pr <= leftPStack[-1]):
             daysToDeploy += 1
+        # 3
         else:
             answer.append(daysToDeploy)
             leftPStack.append(pr)
             daysToDeploy = 1
     answer.append(daysToDeploy)
+    
+    # 4
     return answer
