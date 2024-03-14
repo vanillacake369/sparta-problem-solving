14b903492bce98cd7675f84c8dcf48f1c6e04f93
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-03-14 17:14:38 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,6 @@
 def solution(array, commands):
     answer = []
-    for com in commands:
-        arr = array[com[0]-1 : com[1]]
-        arr.sort()
-        answer.append(arr[com[2]-1])
+    for command in commands:
+        sortedArr = sorted(array[command[0]-1 : command[1]])
+        answer.append(sortedArr[command[2] - 1])
     return answer
