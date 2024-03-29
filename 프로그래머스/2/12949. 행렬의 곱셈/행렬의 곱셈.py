86339911efcf0dec4198f6891da2e0b65ba6e4d9
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-03-29 12:12:47 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.20 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,29 +1,14 @@
 def solution(arr1, arr2):
-    answer = []
-    m, n, r = len(arr1), len(arr1[0]), len(arr2[0])
+    r1, c1 = len(arr1), len(arr1[0])
+    r2, c2 = len(arr2), len(arr2[0])
 
-    for i in range(m):
-        arr = arr1[i]
-        result = []
-        for j in range(r):
-            hap = 0
-            for k in range(n):
-                hap += arr[k] * arr2[k][j]
-            result.append(hap)
-        answer.append(result)
+    result = [[0] * c2 for _ in range(0, r1)]
 
-    return answer
+    # arr1 의 각 행과 arr2 의 각 열에 대해
+    for i in range(0, r1):
+        for j in range(0, c2):
+            # 두 행렬의 데이터를 곱한 결과를 더해줌
+            for k in range(0, c1):
+                result[i][j] += arr1[i][k] * arr2[k][j]
 
-# Ver1
-# def solution(arr1, arr2):
-#     m, n, r = len(arr1), len(arr1[0]), len(arr2[0])
-#     answer = [[0 for _ in range(r)] for _ in range(m)] # m * r 크기의 행렬
-#     for i in range(m):
-#         for k in range(r):
-#             for j in range(n):
-#                 answer[i][k] += arr1[i][j] * arr2[j][k]
-#     return answer
-
-# Ver2
-# def solution(arr1, arr2):
-#     return [[sum(i*j for i, j in zip(row, col)) for col in zip(*arr2)] for row in arr1
+    return result
