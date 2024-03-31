8494cdebdae5de9cde9b7fe2867e41d748b51a54
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-04-01 01:52:16 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 10.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 15:58:29
+2024년 04월 01일 01:52:13
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,14 +1,16 @@
+from collections import deque
+
+
 def solution(t, p):
-    pL = len(p)
-    tN = int(t)
-    pN = int(p)
+    answer = 0
+    slices = deque()
+    pInt = int(p)
+    for i in range(len(t)):
+        if (i + len(p) <= len(t)):
+            slices.append(int(t[i:i + len(p)]))
+    slices = list(slices)  # deque 는 꼭 list 로 변환해주어야한다
     count = 0
-    i = 0
-    while i <= len(t)-pL:
-        num = int(t[i:i+pL])
-        if(num <= pN):
+    for slice in slices:
+        if slice <= pInt:
             count += 1
-        i+=1
-
     return count
-
