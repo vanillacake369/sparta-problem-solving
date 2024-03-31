251ada6f41d564ab9ac7537b8e9d54b99e75df49
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-04-01 01:43:32 +0900

    [level 2] Title: 주식가격, Time: 16.20 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,13 @@
-from collections import deque
-
+# 길이를 확정한 주식은 이후 계산에서 제외하기
 def solution(prices):
-    answer = []
-    priceQueue = deque(prices)
-
-    while priceQueue:
-        price = priceQueue.popleft()
-        flag = 0
-        for p in priceQueue:
-            flag += 1
-            if price > p:
-                break
-        answer.append(flag)
-    
+    answer = [0] * len(prices)
+    comparePriceIndexStack = [0]
+    for i in range(1, len(prices)):
+        while comparePriceIndexStack and prices[comparePriceIndexStack[-1]] > prices[i]:
+            j = comparePriceIndexStack.pop()
+            answer[j] = i - j
+        comparePriceIndexStack.append(i)
+    while comparePriceIndexStack:
+        j = comparePriceIndexStack.pop()
+        answer[j] = len(prices) - 1 - j
     return answer
