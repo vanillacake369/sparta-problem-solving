d874e559ad2f50a2bb37b6d8397357addcf957af
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-04-01 22:59:33 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,11 +1,3 @@
 def solution(s, skip, index):
     answer = ''
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
-    for sk in skip :
-        alpha = alpha.replace(sk, '')
-    for c in s :
-        change = alpha.find(c) + index
-        while change >= len(alpha) :
-            change -= len(alpha)
-        answer += alpha[change]
     return answer
