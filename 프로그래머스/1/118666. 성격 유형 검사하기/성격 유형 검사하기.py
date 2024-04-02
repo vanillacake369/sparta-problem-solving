8c5cb6580bb89d6b73bb71b6bda9dc499a8b9696
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-04-02 11:01:40 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.44 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,17 +1,50 @@
-# https://velog.io/@yujeongkwon/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-PYTHON-%EC%84%B1%EA%B2%A9-%EC%9C%A0%ED%98%95-%EA%B2%80%EC%82%AC%ED%95%98%EA%B8%B0
+def isLeftSide(choice):
+    if choice < 4:
+        return True
+    return False
+
+
+def getResultStrOf(dict):
+    dictValues = list(dict.values())
+    if dictValues[0] == dictValues[1]:
+        alphaBetAsc = list(sorted(dict.items(), key=lambda x: x[0]))
+        return alphaBetAsc[0][0]
+    else:
+        scoreDesc = list(sorted(dict.items(), key=lambda x: x[1], reverse=True))
+        return scoreDesc[0][0]
+
 
 def solution(survey, choices):
     answer = ''
-    dic= {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
-    
-    for s,c in zip(survey,choices):
-        if c>4:     dic[s[1]] += c-4
-        elif c<4:   dic[s[0]] += 4-c
-    
-    li = list(dic.items())
-    
-    for i in range(0,8,2):
-        if li[i][1] < li[i+1][1]: answer += li[i+1][0]
-        else:   answer += li[i][0]
-        
+    # R,T/F,C/M,J,/A,N
+    score = [0, 3, 2, 1, 0, 1, 2, 3]
+    dictAN = {'A': 0, 'N': 0}
+    dictRT = {'R': 0, 'T': 0}
+    dictCF = {'C': 0, 'F': 0}
+    dictMJ = {'M': 0, 'J': 0}
+    for idx, choice in enumerate(choices):
+        choiceScore = score[choice]
+        firstLetterOfEachSurvey = survey[idx][0]
+        secondLetterOfEachSurvey = survey[idx][1]
+        if firstLetterOfEachSurvey == 'A' or firstLetterOfEachSurvey == 'N':
+            if isLeftSide(choice):
+                dictAN[firstLetterOfEachSurvey] += choiceScore
+            else:
+                dictAN[secondLetterOfEachSurvey] += choiceScore
+        if firstLetterOfEachSurvey == 'R' or firstLetterOfEachSurvey == 'T':
+            if isLeftSide(choice):
+                dictRT[firstLetterOfEachSurvey] += choiceScore
+            else:
+                dictRT[secondLetterOfEachSurvey] += choiceScore
+        if firstLetterOfEachSurvey == 'C' or firstLetterOfEachSurvey == 'F':
+            if isLeftSide(choice):
+                dictCF[firstLetterOfEachSurvey] += choiceScore
+            else:
+                dictCF[secondLetterOfEachSurvey] += choiceScore
+        if firstLetterOfEachSurvey == 'M' or firstLetterOfEachSurvey == 'J':
+            if isLeftSide(choice):
+                dictMJ[firstLetterOfEachSurvey] += choiceScore
+            else:
+                dictMJ[secondLetterOfEachSurvey] += choiceScore
+    answer = getResultStrOf(dictRT) + getResultStrOf(dictCF) + getResultStrOf(dictMJ) + getResultStrOf(dictAN)
     return answer
