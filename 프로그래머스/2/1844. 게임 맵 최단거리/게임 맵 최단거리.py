4baa101f8ba5658f3d3c79f962fb28e9ead4e45e
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-04-11 13:22:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,23 +1,28 @@
 from collections import deque
 
+
 def solution(maps):
     answer = 0
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    q = deque()
+    q.append((0, 0))
+
+    while q:
+        x, y = q.popleft()
+        for i in range(0, 4):
+            nextX = x + dx[i]
+            nextY = y + dy[i]
+            n = len(maps)
+            m = len(maps[0])
+            if 0 <= nextX < n and 0 <= nextY < m and maps[nextX][nextY] == 1:
+                maps[nextX][nextY] = maps[x][y] + 1
+                q.append((nextX, nextY))
+                
+    answer = maps[n - 1][m - 1]
     
-    n = len(maps)
-    m = len(maps[0])
+    if answer ==1:
+        answer = -1
 
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-
-    queue = deque([(0,0)])
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx and nx<n and 0<=ny and ny<m and maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx,ny))
-    answer = -1 if maps[n-1][m-1] == 1 else  maps[n-1][m-1]
     return answer
