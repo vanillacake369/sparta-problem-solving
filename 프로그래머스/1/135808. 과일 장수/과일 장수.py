ecf469324b9fd27881221916f9ebcfd5edf26012
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-04-15 11:54:17 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,8 @@
-# 역순정렬을 한다
-# k점수 이하의 사과 중 최대값의 사과 m개를 추출한다.
-# 각 박스 별 점수를 합한다.
 def solution(k, m, score):
     answer = 0
-    score.sort(reverse = True)
-    for i in range(0,len(score),m):
-        box = score[i:i+m]
+    score.sort(reverse=True)
+    for i in range(0, len(score), m):
+        box = score[i:i + m]
         if len(box) == m:
-            answer += min(box) * m
+            answer += box[m - 1] * m
     return answer
